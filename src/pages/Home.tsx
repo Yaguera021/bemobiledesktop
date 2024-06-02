32620fd63b0c36037ec59281c75167e75938f4a8
Seu nome <yago_bohmcruz@hotmail.com>
2024-06-01 23:22:50 -0300

    Cria o Header e seu estilo

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,5 @@
+import Header from "../components/Header";
+
 export default function Home() {
-  return (
-    <div>
-      <h1>Be Mobile</h1>
-      <h2>Front-End</h2>
-    </div>
-  );
+  return <Header />;
 }
