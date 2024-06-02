598dc6b8730da4bed59ad5ca54920d59bdc89f1d
Seu nome <yago_bohmcruz@hotmail.com>
2024-06-02 02:22:52 -0300

    Cria a função para fazer a requisição as dados dos funcionários

---
+++ src/data/api.js
@@ -0,0 +1,13 @@
+export default async function fetchEmployees() {
+  try {
+    const response = await fetch("http://localhost:3000/employees");
+    if (!response.ok) {
+      throw new Error("Failed to fetch employees");
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching employees:", error);
+    return [];
+  }
+}
