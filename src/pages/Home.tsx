ea2278df0ca571ed7cd58f33d13d06304a452209
Seu nome <yago_bohmcruz@hotmail.com>
2024-06-02 21:45:12 -0300

    Implementa funcionalidade de pesquisa e estilos para desktop :sparkles:

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,4 @@
 import Header from "../components/Header";
-import SearchBar from "../components/SearchBar";
 import Table from "../components/Table";
 
 import "./styles/Home.scss";
@@ -9,10 +8,6 @@ export default function Home() {
     <div className='home-wrapper-content'>
       <Header />
       <section>
-        <div className='top-wrapper'>
-          <h4>Funcionários</h4>
-          <SearchBar />
-        </div>
         <Table />
       </section>
     </div>
